eb0e95f2b3b50c835413939781bade1535e68499
Sebastian Richardson <sebastian.nicolasrg24@outlook.com>
2024-03-06 14:59:23 -0500

    Create timeConversion.java

---
+++ timeConversion.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class timeConversion {
+    public static void main (String[] args){
+        Scanner userInput = new Scanner(System.in);
+        
+        System.out.println("Enter time in minutes: ");
+        int timeInMin = userInput.nextInt();
+        int onesMin = timeInMin % 60;
+        int tensMin = onesMin / 10;
+        int timeInHours = (timeInMin - onesMin)/ 60;
+        userInput.close();
+        
+        System.out.println("The time in hours is " + timeInHours + ":" + tensMin + onesMin);
+
+    }
+}
